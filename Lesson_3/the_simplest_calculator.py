cbcaf6ee16b756928bb3aa5ad01466f6f2e4108f
pavelhaida <areas-pk@hotmail.com>
2025-02-25 19:53:32 +0200

    update calculator

--- Lesson_3/the_simplest_calculator.py
+++ Lesson_3/the_simplest_calculator.py
@@ -1,24 +1,32 @@
+#  модифікація калькулятора для завдання 5.2
+import os
 
-listsdictf = ['\nВведіть перше число: ', '\nВедіть операцію: ', '\nВведіть друге число: ']
-listoperf = [' ', ' ', ' ']
-
-for i in range(len(listsdictf)):
-    listoperf[i] = input(listsdictf[i])
-
-if listoperf[1] == "+":
-    rez = int(listoperf[0]) + int(listoperf[2])
-    print(f"{listoperf[0]} {listoperf[1]} {listoperf[2]} = {rez}")
-elif listoperf[1] == "-":
-    rez = int(listoperf[0]) - int(listoperf[2])
-    print(f"{listoperf[0]} {listoperf[1]} {listoperf[2]} = {rez}")
-elif listoperf[1] == "*":
-    rez = int(listoperf[0]) * int(listoperf[2])
-    print(f"{listoperf[0]} {listoperf[1]} {listoperf[2]} = {rez}")
-elif listoperf[1] == "/" and int(listoperf[2]) != 0:
-    rez = int(listoperf[0]) / int(listoperf[2])
-    print(f"{listoperf[0]} {listoperf[1]} {listoperf[2]} = {rez}")
-else:
-    print("""Не вірно обрана операція!!\n
-             Лише допустимі такі дії: +, -, *, / \n
-             або ви вирішили поділити на 0""")
+while True:
+    listsdictf = ['\nВведіть перше число: ', '\nВедіть операцію: ', '\nВведіть друге число: ']
+    listoperf = [' ', ' ', ' ']
     
+    for i in range(len(listsdictf)):
+        listoperf[i] = input(listsdictf[i])
+    
+    if listoperf[1] == "+":
+        rez = int(listoperf[0]) + int(listoperf[2])
+        print(f"{listoperf[0]} {listoperf[1]} {listoperf[2]} = {rez}")
+    elif listoperf[1] == "-":
+        rez = int(listoperf[0]) - int(listoperf[2])
+        print(f"{listoperf[0]} {listoperf[1]} {listoperf[2]} = {rez}")
+    elif listoperf[1] == "*":
+        rez = int(listoperf[0]) * int(listoperf[2])
+        print(f"{listoperf[0]} {listoperf[1]} {listoperf[2]} = {rez}")
+    elif listoperf[1] == "/" and int(listoperf[2]) != 0:
+        rez = int(listoperf[0]) / int(listoperf[2])
+        print(f"{listoperf[0]} {listoperf[1]} {listoperf[2]} = {rez}")
+    else:
+        print("""Не вірно обрана операція!!\n
+                Лише допустимі такі дії: +, -, *, / \n
+                або ви вирішили поділити на 0""")
+    
+    question = input("Якщо хочете продовдити роботу введіть 'так' або 'т': ")
+    if question == 'так' or question == 'т':
+        os.system('clear')
+    else:
+        break
---
+++ Lesson_5/variable_name.py
@@ -0,0 +1,7 @@
+#import keyword
+
+
+str_chek = '!"#$%&\'()*+,-./:;><?=@[\\]^`{|}~ '
+str_en_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+str_ru_alphabet = 'АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ'
+str_number = '1234567890'
